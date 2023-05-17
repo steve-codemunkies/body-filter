6d8dcb1f7fb04d23a1951c274e7b7b82d6bbaf0b
Steve Hocking <steve@codemunki.es>
2023-05-17 14:32:41 +0000

    Initialise CDK application

---
+++ test/find-an-sqs-event.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FindAnSqsEvent from '../lib/find-an-sqs-event-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/find-an-sqs-event-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FindAnSqsEvent.FindAnSqsEventStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
